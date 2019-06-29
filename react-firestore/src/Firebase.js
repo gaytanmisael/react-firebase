5c2123e1f9969d435ccd88ecaf3b1fe1375d2e58
Misael Gaytan <gaytanmisael@gmail.com>
2019-06-29 15:23:37 -0700

    Update Firebase.js

--- react-firestore/src/Firebase.js
+++ react-firestore/src/Firebase.js
@@ -4,12 +4,12 @@ import firestore from 'firebase/firestore';
 const settings = {timestampsInSnapshots: true};
 
 const config = {
-    apiKey: "AIzaSyCEr5r27atJpwAdwiZPHl3ZgMjdUtgKgcY",
-    authDomain: "firestore-90512.firebaseapp.com",
-    databaseURL: "https://firestore-90512.firebaseio.com",
-    projectId: "firestore-90512",
-    storageBucket: "firestore-90512.appspot.com",
-    messagingSenderId: "140654601471"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   };
 
 firebase.initializeApp(config);
